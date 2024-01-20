d87fc4453217994adbe34bb738fc1c22496896a4
Ashish <115717632+ashish-54321@users.noreply.github.com>
2024-01-21 03:42:24 +0530

    Add files via upload

---
+++ index (1).js
@@ -0,0 +1,35 @@
+const express = require('express');
+const cors = require("cors")
+const multer = require('multer');
+const Image = require("./db/Image")
+const config = require('./db/config')
+const app = express();
+app.use(express.json())
+app.use(cors());
+
+// Set up Multer for file uploads
+const storage = multer.memoryStorage(); // Store the image data in memory
+const upload = multer({ storage: storage });
+
+
+
+app.post('/upload', upload.single('image'), async (req, res) => {
+    if (!req.file) {
+        return res.status(400).send('No file uploaded.');
+    }
+
+    const image = new Image({
+        name: req.body.name,
+        data: req.file.buffer,
+        contentType: req.file.mimetype,
+    });
+
+    try {
+        await image.save();
+        res.send('Image uploaded successfully.');
+    } catch (error) {
+        res.status(500).send('Error uploading image.');
+    }
+});
+
+app.listen(4000)
